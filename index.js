263bcfbfbee95bb54afbafb81aca08441dc7998c
qtcoder999 <30284500+qtcoder999@users.noreply.github.com>
2018-11-21 17:29:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 class List extends React.Component {
   constructor(props) {
     super(props);
+    this.delete = this.delete.bind(this);
   }
-
+  
+  delete(index){
+    //alert("Deleted Key")
+    //alert("Deleted Key" + this.props.key)
+    //console.log(this.props);
+    //console.log(index);
+    this.props.deleteItem(index)
+    // alert("Deleted Key")
+  } 
+    
   render() {
     //console.log("success")
     return (
         <ul>
           {
-            this.props.items.map((item, index) => <li key={index}>{item}</li>)
+            this.props.items.map((item, index) => <li key={index} onClick={() => this.delete(index)}>{item}</li>)
           }
         </ul>
     );
@@ -22,6 +32,8 @@ class App extends React.Component {
       term: '',
       items: []
     };
+    this.del = this.del.bind(this);
+
   }
 
   onChange = (event) => {
@@ -30,21 +42,36 @@ class App extends React.Component {
 
   onSubmit = (event) => {
     event.preventDefault();
-    this.setState({
-      term: '',
-      items: [...this.state.items, this.state.term]
-    });
+        if( this.state.term !== ''){
+            this.setState({
+              term: '',
+              items: [...this.state.items, this.state.term]
+            });
+        }
+  }
+ //varun
+  del(key){
+     console.log("reached in parent");
+     //console.log(key);
+    this.state.items.splice(key, 1)
+    //console.log(newItems);
+     this.setState({
+       term: '',
+       items: [...this.state.items]
+     });
+    
+    //console.log(this.state.items); 
+    //console.log(this.state);
   }
 
   render() {
     return (
       <div>
-      
         <form className="App" onSubmit={this.onSubmit}>
           <input value={this.state.term} onChange={this.onChange} />
           <button>Submit</button>
         </form>
-        <List items={this.state.items} />
+        <List deleteItem={this.del} items={this.state.items} />
       </div>
     );
   }
